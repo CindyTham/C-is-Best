800179b79fd00aef593c235f2df7aef1277e33ec
Cindy Tham <cindytham@hotmail.com>
2020-12-06 18:17:16 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+std::cout << "            C\n";
+std::cout << "          i   I\n";
+std::cout << "        s       s\n";
+std::cout << "      b           b\n";
+std::cout << "    e               e\n";
+std::cout << "  s                   s\n";
+std::cout << "t s e b s i C i s b e s t\n";
+
+}
